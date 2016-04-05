857aca31473b00415b36c323c30093bb011c38c6
sokasyn <sokasyn@qq.com>
2016-04-05 16:05:26 +0800

    project created.

---
+++ MemoryBook/MemoryBook/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MemoryBook
+//
+//  Created by Samson Tseng on 16/4/5.
+//  Copyright © 2016年 Samson Tseng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MemoryBook/MemoryBook/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MemoryBook
+//
+//  Created by Samson Tseng on 16/4/5.
+//  Copyright © 2016年 Samson Tseng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
