999b0694bea9f869355f6aa3c020c98ab68cb411
sokasyn <sokasyn@qq.com>
2016-04-05 16:13:48 +0800

    工程的用途简介

--- MemoryBook/MemoryBook/AppDelegate.h
+++ MemoryBook/MemoryBook/AppDelegate.h
@@ -5,6 +5,10 @@
 //  Created by Samson Tseng on 16/4/5.
 //  Copyright © 2016年 Samson Tseng. All rights reserved.
 //
+/*
+ 用于纪录在编码过程纪录知识点，经验，积极遇到的问题及解决办法。
+ 好记性不如烂笔头！
+ */
 
 #import <UIKit/UIKit.h>
 
