c199eabed6627cd476442999968873d424841e1e
sokasyn <sokasyn@qq.com>
2016-06-03 18:41:33 +0800

    ..

--- MemoryBook/MemoryBook/Common/Helpers/DatabaseHelper.h
+++ MemoryBook/MemoryBook/Common/Helpers/DatabaseHelper.h
@@ -10,4 +10,32 @@
 
 @interface DatabaseHelper : NSObject
 
+#pragma mark - database
+// 创建数据库
++ (BOOL)createDatabase:(NSString *)databaseName;
+
+// 检查数据库是否存在
++ (BOOL)isExistDatabase:(NSString *)databaseName;
+
+// 删除数据库
++ (void)deleteDatabase:(NSString *)databaseName;
+
+
+#pragma mark - Table
+// 创建数据表
++ (void)createTable:(NSString *)tableName;
+
+// 检查数据表是否存在
++ (BOOL)isExistTalbe:(NSString *)tableName;
+
+// 插入表数据
++ (void)insertTable:(NSString *)tableName withSql:(NSString *)sql;
+
+// 更新表数据
++ (void)updataTable:(NSString *)tableName withSql:(NSString *)sql;
+
+// 删除数据表
++ (void)deleteTable:(NSString *)tableName;
+
+
 @end
